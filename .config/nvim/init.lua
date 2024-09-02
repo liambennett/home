cd761a6b0fecde8efb9ff1856ad384e3155c2f39
Liam Bennett <liampbennett@gmail.com>
2024-09-02 23:50:00 +0100

    initial set of stow files

---
+++ .config/nvim/init.lua
@@ -0,0 +1,24 @@
+vim.g.mapleader = " "
+vim.g.netrw_banner = 0
+
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_ruby_provider = 0
+
+vim.opt.swapfile = false
+vim.opt.termguicolors = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.backup = false
+vim.opt.cc = "80"
+vim.opt.scrolloff = 10
+vim.opt.syntax = "on"
+
