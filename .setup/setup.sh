d43a49e48813143728230ac3e48fed0984eba98a
Liam Bennett <liampbennett@gmail.com>
2025-01-25 09:49:22 +0000

    add gitconfig and setup scripts

---
+++ .setup/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt install git yq -y
+sudo snap install nvim --classic
+sudo snap install code --classic
+sudo snap install alacritty --classic
+sudo snap install helm --classic
+
